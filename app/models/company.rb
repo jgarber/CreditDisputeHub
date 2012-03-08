954b11913a9f195524d27cd708daede2ddef9600
Jason Garber <jg@jasongarber.com>
2012-03-07 21:23:36 -0500

    Validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,3 @@
 class Company < ActiveRecord::Base
+  validate :name, :presence => true
 end
--- app/models/dispute.rb
+++ app/models/dispute.rb
@@ -2,5 +2,5 @@ class Dispute < ActiveRecord::Base
   belongs_to :company
   accepts_nested_attributes_for :company
 
-  validates :company, :presence => true
+  validates :company, :title, :description, :presence => true
 end
