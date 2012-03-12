e0f943c4084ab65eba2d77c77e0e00fa6cf440d5
Jason Garber <jg@jasongarber.com>
2012-03-12 11:44:54 -0400

    Mass-attribute protection

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,5 @@
 class Company < ActiveRecord::Base
   validate :name, :presence => true
   has_many :disputes, :dependent => :destroy
+  attr_accessible :name, :department, :phone, :fax, :address, :email, :category
 end
--- app/models/dispute.rb
+++ app/models/dispute.rb
@@ -1,6 +1,8 @@
 class Dispute < ActiveRecord::Base
   belongs_to :company
   accepts_nested_attributes_for :company
+  attr_accessible :title, :company_id, :disputed_amount, :description,
+    :private, :company_attributes
 
   validates :company, :title, :description, :presence => true
 end
